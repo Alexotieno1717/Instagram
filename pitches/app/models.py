6736972e4a8eb976ff9cbbc45b79db4d4632523e
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-21 11:52:40 +0300

    Install flask bootstrap

--- pitches/app/models.py
+++ pitches/app/models.py
@@ -8,11 +8,13 @@ from app import db
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), index=True)
-    email = db.Column(db.String(255), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
 
 
 @login_manager.user_loader
