71a59ea01c08871270d86f22c48ce92cd485b5e2
Alexotieno1717 <alexotieno900@gmail.com>
2020-10-21 10:10:06 +0300

    added start

---
+++ start.sh
@@ -0,0 +1,8 @@
+SECRET_KEY=u9^$069o51#hc6%on$c7j+*y5u@4i0zgdy39gj5uq^hu$a@oj$
+ALLOWED_HOST=*
+DEBUG=False
+DB_NAME=d5iqmjo2nu7hh8
+DB_USER=pfqjsoxsyxopbx
+DB_PASSWORD=1dece8ec6d6b4bb9f9a9d324d51bd93ea5f78f688ffa397f60b815219852de33
+DB_PORT=5432
+DB_HOST=ec2-54-157-4-216.compute-1.amazonaws.com
